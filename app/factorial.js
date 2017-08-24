c9e15e56c63c194d56c4576f8c1b81709fc3decc
Marina Timoshenko <nemascha@gmail.com>
2017-08-24 14:29:32 +0300

    ref else if

--- app/factorial.js
+++ app/factorial.js
@@ -2,11 +2,14 @@ module.exports.factorial = function(n) {
 	
 	if (n == 0) {
 		return 1;
-	}
+	} else if (n < 0) {
+		return false;
+	} else {
 
-	var num = 1;
-	for (var i = 1; i < n + 1; i++) {
+		var num = 1;
+		for (var i = 1; i < n + 1; i++) {
 		num *=i;
+		}
 	}
 	return num;
 };
